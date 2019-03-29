4e93241e96ad179c959874d0bbe3ef1bd4a7fe4e
Liam Golightley <liamgolightley@liams-mbp.home>
2019-03-28 22:00:21 -0400

    update add teammates page

---
+++ src/app/invite-team-mates/invite-team-mates.page.spec.ts
@@ -0,0 +1,27 @@
+// import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { InviteTeamMatesPage } from './invite-team-mates.page';
+
+// describe('InviteTeamMatesPage', () => {
+//   let component: InviteTeamMatesPage;
+//   let fixture: ComponentFixture<InviteTeamMatesPage>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ InviteTeamMatesPage ],
+//       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(InviteTeamMatesPage);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
