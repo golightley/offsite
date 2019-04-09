9f9ebca54a579c8e70c47007326f55143a8eb59c
JH Lee <jhpassion0621@hotmail.com>
2019-04-10 04:21:52 +0800

    complete feedback UI

---
+++ src/app/feedback/feedback-content/feedback-content.page.ts
@@ -0,0 +1,65 @@
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {SurveyServiceService} from '../../services/survey-service.service';
+import {FeedbackCategoryModel, TeammatesModel} from './feedback-content.model';
+import * as firebase from 'firebase/app';
+
+require('firebase/auth');
+
+@Component({
+  selector: 'app-feedback-content',
+  templateUrl: './feedback-content.page.html',
+  styleUrls: ['./feedback-content.page.scss'],
+})
+export class FeedbackContentPage {
+  page = 'what';
+  categories: FeedbackCategoryModel[] = [];
+  teammates: TeammatesModel[] = [];
+
+  constructor(private surveyService: SurveyServiceService,
+              private router: Router) {
+    this.getFeedbackQuestions();
+    this.getTeamMembers();
+  }
+
+  private getFeedbackQuestions() {
+    // get notification data from the survey service
+    this.surveyService.getFeedbackCategories()
+      .then(docs => {
+        docs.forEach(doc => {
+          if (doc.data().hasOwnProperty('name')) {
+            this.categories.push(new FeedbackCategoryModel(doc.data()['name']));
+          }
+        });
+        console.log(this.categories);
+    });
+  }
+
+  private getTeamMembers() {
+    let userId = 'rFeEWfEvUOQMvrp7LADUEPKt3U23';
+    if (firebase.auth().currentUser && firebase.auth().currentUser.uid) {
+      userId = firebase.auth().currentUser.uid;
+    }
+
+    this.surveyService.getTeamMembers(userId)
+      .then(docs => {
+        docs.forEach(doc => {
+          this.teammates.push(new TeammatesModel(doc.data()));
+        });
+        console.log(this.teammates);
+    });
+  }
+
+  checkSubmitStatus() {
+    return 0 === this.categories.filter(category => {
+             return category.checked;
+           }).length
+           || 0 === this.teammates.filter(teammate => {
+             return teammate.checked;
+           }).length;
+  }
+
+  submitFeedback() {
+    this.router.navigateByUrl('app/feedback/feedback-request');
+  }
+}
