1fe78fcc083ab8753b6ef7fccfa43d9c0507c2eb
WangWei <wangwei_0610@outlook.com>
2019-05-23 01:03:08 +0800

    Updated get team data in loadIdeas()

--- src/app/ideas/ideas.page.ts
+++ src/app/ideas/ideas.page.ts
@@ -10,7 +10,6 @@ import { PopoverController } from '@ionic/angular';
 import { PopoverReportComponent } from '../components/popover-report/popover-report.component';
 
 import { LoadingService } from '../services/loading-service';
-import { async } from '@angular/core/testing';
 require('firebase/auth');
 @Component({
   selector: 'app-ideas',
@@ -65,27 +64,18 @@ export class IdeasPage implements OnInit {
   }
 
   async loadIdeas() {
-    
+
     console.log('== load Ideas ==');
     await this.loadingService.doFirebase(async () => {
-      let team = '';
+
       const that = this;
       that.ideas = [];
       if (typeof firebase.auth === 'function') {
-        firebase.auth().onAuthStateChanged(user => {
-          if (user) {
-            
-          }
-        });
-        const docRef = await firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid);
-        await docRef.get().then(async(doc) => {
-          if (doc.exists) {
+        const teamData = await this.surveyService.getTeamByUserId(firebase.auth().currentUser.uid);
+        if (teamData.exists) {
             // first fetch the team ID
-            console.log('Team data:', doc.data().team);
-            console.log('User data:', doc.data());
-
-            team = doc.data().teamId;
-            that.teamId = team;
+            console.log('Selected Team Name:', teamData.data().teamName);
+            that.teamId = teamData.id;
             if (that.teamId === undefined) {
               console.log('No selected team! returned');
               return;
@@ -93,7 +83,7 @@ export class IdeasPage implements OnInit {
             // now get the ideas based on that team
             const query = await firebase.firestore().collection('ideas')
               // .where('team', '==', 'E4ZWxJbFoDE29ywISRQY')
-              .where('team', '==', team)
+              .where('team', '==', that.teamId)
               .where('reported', '==', false)
               .orderBy('score', 'desc')
               .orderBy('timestamp', 'asc');
@@ -116,9 +106,6 @@ export class IdeasPage implements OnInit {
             // doc.data() will be undefined in this case
             console.log('No such document!');
           }
-        }).catch(function (error) {
-          console.log('Error getting document:', error);
-        });
       }
     });
 
@@ -172,7 +159,7 @@ export class IdeasPage implements OnInit {
       userId: firebase.auth().currentUser.uid,
       action: 'upvote'
     };
-    const result =  await this.loadingService.doFirebase(async () => {
+    const result = await this.loadingService.doFirebase(async () => {
       const resp = await this.http.post('https://us-central1-offsite-9f67c.cloudfunctions.net/updateIdeaScore', JSON.stringify(body), {
         responseType: 'text'
       }).toPromise();
