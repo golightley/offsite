d8db7022cbef5980a90dbcb72cd859c4e534532f
WangWei0610 <wangwei91610@hotmail.com>
2019-05-30 13:26:33 -0700

    modify code format.

--- src/app/pages/team/select-team/select-team.component.ts
+++ src/app/pages/team/select-team/select-team.component.ts
@@ -19,76 +19,26 @@ export class SelectTeamComponent implements OnInit {
   color: string = '';
 
   constructor(
-    private pc : PopoverController,
+    private pc: PopoverController,
     public loadingService: LoadingService,
     private navParams: NavParams,
     public router: Router,
     private menu: MenuController
-  )
-  {
+  ) {
 
   }
 
-  ngOnInit()
-  {
-    
+  ngOnInit() {
     this.userTeams = this.navParams.get('userTeams');
     this.activeTeam = this.navParams.get('activeTeam');
     console.log(this.activeTeam);
-    //this.userId = firebase.auth().currentUser.uid;
-    //this.getCurTeam();
     firebase.auth().onAuthStateChanged(user => {
       this.userId = user.uid;
-      //this.getCurTeam();
-    })
-  }
-  async getCurTeam()
-  {
-    const that = this;
-    
-    await this.loadingService.doFirebase(async () => {
-      const docRef = firebase.firestore().collection('users').doc(that.userId);
-      docRef.onSnapshot((snapshot) => {
-        
-            // console.log(change.doc.data());
-            const userData = new UserModel(snapshot.data());
-            that.activeTeam = userData.teamId;
-      });
-    })
+    });
   }
 
-  async getUserTeams()
-  {
-    const that = this;
-    await this.loadingService.doFirebase(async () => {
-      console.log('[TeamSwitching] userId = ' + this.userId);
-      const query = await firebase.firestore().collection('teams')
-        .where('members', 'array-contains', { uid: this.userId });
-      query.onSnapshot((snapshot) => {
-        console.log('TeamSwitching Listener attached');
-        // retrieve anything that has changed
-          console.log('[TeamSwitching] team count = ', snapshot.size);
-          const changedDocs = snapshot.docChanges();
-          changedDocs.forEach((change) => {
-            // console.log(change.doc.data());
-            const userTeam = new UserTeamsModel(change.doc.data());
-            userTeam.id = change.doc.id;
-            if (change.oldIndex !== -1) {
-              that.userTeams.splice(change.oldIndex, 1);
-            }
-            if (change.newIndex !== -1) {
-              console.log('[TeamSwitching] doc id = ', change.doc.id);
-              that.userTeams.splice(change.newIndex, 0, userTeam);
-            }
-          });
-      });
-    })
-  }
-
-  async selectTeam(teamdId: string)
-  {
+  async selectTeam(teamdId: string) {
     console.log('[TeamSwitching] selectTeam = ' + teamdId);
-    //this.activeTeam = teamdId;
     await this.loadingService.doFirebase(async () => {
       firebase.firestore().collection('users').doc(this.userId).update({
         'teamId': teamdId,
